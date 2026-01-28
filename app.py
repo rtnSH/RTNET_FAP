f3982f4c314ab2599460d82b3916a016ec82998f
suho <sh.lee@rtnet.co.kr>
2026-01-28 11:26:16 +0900

    first commit

--- app.py
+++ app.py
@@ -97,4 +97,5 @@ def format_issue(issue):
     }
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
